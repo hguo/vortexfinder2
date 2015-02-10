7f4bf0b17f18aa0171d07750644b0c3b5501a51b
Hanqi Guo <hguo@anl.gov>
2015-02-10 10:45:10 -0600

    serialization for punctured edges/faces

--- src/extractor/Puncture.h
+++ src/extractor/Puncture.h
@@ -1,29 +1,39 @@
 #ifndef _PUNCTURE_H
 #define _PUNCTURE_H
 
+#include <map>
+#include <string>
+#include "def.h"
+
 struct PuncturedFace
 {
   int chirality;
   double pos[3];
-  
-  bool visited; 
-
-  PuncturedFace() : visited(false) {}
 };
 
 struct PuncturedEdge
 {
   int chirality; 
   double t; // punctured time
-  
-  bool visited; 
-
-  PuncturedEdge() : visited(false) {}
 };
 
-struct PuncturedElem
+struct PuncturedCell
 {
   int chiralities[6]; // chiralities on faces
 };
+
+//////// I/O for faces
+bool SerializePuncturedFaces(const std::map<FaceIdType, PuncturedFace> m, std::string &buf);
+bool UnserializePuncturedFaces(std::map<FaceIdType, PuncturedFace> &m, const std::string &buf);
+
+bool SavePuncturedFaces(const std::map<FaceIdType, PuncturedFace> m, const std::string &filename);
+bool LoadPuncturedFaces(std::map<FaceIdType, PuncturedFace> &m, const std::string &filename);
+
+//////// I/O for edges
+bool SerializePuncturedEdges(const std::map<EdgeIdType, PuncturedEdge> m, std::string &buf);
+bool UnserializePuncturedEdges(std::map<EdgeIdType, PuncturedEdge> &m, const std::string &buf);
+
+bool SavePuncturedEdges(const std::map<EdgeIdType, PuncturedEdge> m, const std::string &filename);
+bool LoadPuncturedEdges(std::map<EdgeIdType, PuncturedEdge> &m, const std::string &filename);
 
 #endif
