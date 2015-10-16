24e816d9b611c7fb98d97b016dd9a51ef413d4ca
Hanqi Guo <hguo@anl.gov>
2015-10-16 14:27:58 -0500

    added supercurrent filter to plugin.cmake

--- plugin.cmake
+++ plugin.cmake
@@ -1,5 +1,5 @@
 pv_plugin(VortexFinder2
-  PLUGIN_NAMES BDATReader BDATSeriesReader GLGPUVortexFilter
+  PLUGIN_NAMES BDATReader BDATSeriesReader GLGPUVortexFilter GLGPUSupercurrentFilter
   DESCRIPTION "Analyze vorticies in time-dep. Ginzburg-Landau (TDGL) superconductor sim. data"
   AUTOLOAD
   )
